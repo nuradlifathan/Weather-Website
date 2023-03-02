2d1904cb11501cfb250cc4c13b9f68c7c99eb266
RGS <nuradlifathan@gmail.com>
2023-03-02 19:37:18 +0700

    environment variables

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const container = document.querySelector(".container")
-const search = document.querySelector(".search-card button")
+const search = document.querySelector(".search-box button")
 const weatherBox = document.querySelector(".weather-box")
 const weatherDetails = document.querySelector(".weather-details")
-const notFound = document.querySelector(".404")
+const notFound = document.querySelector(".not-found")
 
 search.addEventListener("click", () => {
-  const APIkey = "dcb7dad3f3d4e95de47920c688f32f37"
-  const kota = document.querySelector(".search-card input").value
+  const APIkey = process.env.APIKEY
+  const kota = document.querySelector(".search-box input").value
 
   if (kota === "") return
 
   fetch(
-    `https://api.openweathermap.org/data/3.0/weather?q=${kota}&units=metric&appid=${APIkey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${kota}&units=metric&appid=${APIkey}`
   )
     .then((response) => response.json())
     .then((json) => {
@@ -22,7 +22,7 @@ search.addEventListener("click", () => {
         weatherDetails.style.display = "none"
 
         notFound.style.display = "block"
-        notFound.style.classList.add("fadeIn")
+        notFound.classList.add("fadeIn")
 
         return
       }
@@ -33,5 +33,43 @@ search.addEventListener("click", () => {
       const temperature = document.querySelector(".weather-box .temperature")
       const description = document.querySelector(".weather-box .description")
       const humidity = document.querySelector(".weather-details .humidity span")
+      const wind = document.querySelector(".weather-details .wind span")
+
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png"
+          break
+
+        case "Rain":
+          image.src = "images/rain.png"
+          break
+
+        case "Snow":
+          image.src = "images/snow.png"
+          break
+
+        case "Clouds":
+          image.src = "images/cloud.png"
+          break
+
+        case "Haze":
+          image.src = "images/mist.png"
+          break
+
+        default:
+          image.src = ""
+      }
+
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
+      description.innerHTML = `${json.weather[0].description}`
+      humidity.innerHTML = `${json.main.humidity}%`
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
+
+      weatherBox.style.display = ""
+      weatherDetails.style.display = ""
+      weatherBox.classList.add("fadeIn")
+      weatherDetails.classList.add("fadeIn")
+
+      container.style.height = "590px"
     })
 })
